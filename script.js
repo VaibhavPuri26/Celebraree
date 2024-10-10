d8ed2337c92a1f8d5480ae8c7b220696daeb6daa
Vaibhav Puri <vaibhav.puri26@gmail.com>
2024-10-11 03:16:26 +0530

    Reordering

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ let italicButton = document.getElementById("italic");
 let underlineButton = document.getElementById("underline");
 let undoButton = document.getElementById("undo");
 let redoButton = document.getElementById("redo");
+let textColorInput = document.getElementById("textColor");
+let shuffleButton = document.getElementById("shuffleButton");
 
 let selectedElement = null;
 let history = [];
@@ -16,23 +18,47 @@ let redoStack = [];
 let slides = [
     {
         src: "uploads/Image2.jpeg",
-        text: "Hello World",
-        left: "10px",
-        top: "10px",
+        predefinedText: {
+            text: "Hello World",
+            left: "10px",
+            top: "10px",
+            color: "#000000",
+            fontFamily: "Arial",
+            fontSize: "16px",
+            fontWeight: "normal",
+            fontStyle: "normal",
+            textDecoration: "none"
+        },
         textBoxes: []
     },
     {
         src: "uploads/Image3.jpeg",
-        text: "Celebrare",
-        left: "10px",
-        top: "10px",
+        predefinedText: {
+            text: "Celebrare",
+            left: "10px",
+            top: "10px",
+            color: "#000000",
+            fontFamily: "Arial",
+            fontSize: "16px",
+            fontWeight: "normal",
+            fontStyle: "normal",
+            textDecoration: "none"
+        },
         textBoxes: []
     },
     {
         src: "uploads/Image1.jpg",
-        text: "Internship",
-        left: "10px",
-        top: "10px",
+        predefinedText: {
+            text: "Internship",
+            left: "10px",
+            top: "10px",
+            color: "#000000",
+            fontFamily: "Arial",
+            fontSize: "16px",
+            fontWeight: "normal",
+            fontStyle: "normal",
+            textDecoration: "none"
+        },
         textBoxes: []
     }
 ];
@@ -48,16 +74,15 @@ function updateSlide() {
     let predefinedTextDiv = document.createElement("div");
     predefinedTextDiv.className = "text-box predefined";
     predefinedTextDiv.contentEditable = true;
-    predefinedTextDiv.innerText = slides[currentImageIndex].text;
+    Object.assign(predefinedTextDiv.style, slides[currentImageIndex].predefinedText);
+    predefinedTextDiv.innerText = slides[currentImageIndex].predefinedText.text;
     predefinedTextDiv.style.position = "absolute";
-    predefinedTextDiv.style.left = slides[currentImageIndex].left;
-    predefinedTextDiv.style.top = slides[currentImageIndex].top;
     imageContainer.appendChild(predefinedTextDiv);
 
     makeElementDraggable(predefinedTextDiv, true);
 
     predefinedTextDiv.addEventListener("input", () => {
-        slides[currentImageIndex].text = predefinedTextDiv.innerText;
+        slides[currentImageIndex].predefinedText.text = predefinedTextDiv.innerText;
         saveState();
     });
 
@@ -65,20 +90,20 @@ function updateSlide() {
         let newTextBox = document.createElement("div");
         newTextBox.contentEditable = true;
         newTextBox.className = "text-box";
-        newTextBox.innerText = textBox.innerText;
-        newTextBox.style.fontFamily = textBox.fontFamily;
-        newTextBox.style.fontSize = textBox.fontSize;
+        Object.assign(newTextBox.style, textBox);
+        newTextBox.innerText = textBox.text;
         newTextBox.style.position = "absolute";
-        newTextBox.style.left = textBox.left; 
-        newTextBox.style.top = textBox.top; 
         imageContainer.appendChild(newTextBox);
         makeElementDraggable(newTextBox);
 
         newTextBox.addEventListener("input", () => {
             saveState();
-            textBox.innerText = newTextBox.innerText;
-            textBox.left = newTextBox.style.left;
-            textBox.top = newTextBox.style.top;
+            updateTextBoxData(newTextBox, textBox);
+        });
+
+        newTextBox.addEventListener("click", (e) => {
+            e.stopPropagation();
+            selectElement(newTextBox);
         });
     });
 
@@ -86,25 +111,39 @@ function updateSlide() {
         e.stopPropagation();
         selectElement(predefinedTextDiv);
     });
+
+    updateControls();
+}
+
+function updateTextBoxData(element, data) {
+    data.text = element.innerText;
+    data.left = element.style.left;
+    data.top = element.style.top;
+    data.fontFamily = element.style.fontFamily;
+    data.fontSize = element.style.fontSize;
+    data.color = element.style.color;
+    data.fontWeight = element.style.fontWeight;
+    data.fontStyle = element.style.fontStyle;
+    data.textDecoration = element.style.textDecoration;
 }
 
 function saveCurrentTextBoxes() {
-    const textBoxes = document.querySelectorAll('.text-box');
-    textBoxes.forEach(tb => {
-        const index = slides[currentImageIndex].textBoxes.findIndex(textBox => textBox.innerText === tb.innerText);
-        if (index !== -1) {
-            slides[currentImageIndex].textBoxes[index].innerText = tb.innerText;
-            slides[currentImageIndex].textBoxes[index].left = tb.style.left;
-            slides[currentImageIndex].textBoxes[index].top = tb.style.top;
-            slides[currentImageIndex].textBoxes[index].fontFamily = tb.style.fontFamily;
-            slides[currentImageIndex].textBoxes[index].fontSize = tb.style.fontSize;
-        }
-    });
+    const textBoxes = document.querySelectorAll('.text-box:not(.predefined)');
+    slides[currentImageIndex].textBoxes = Array.from(textBoxes).map(tb => ({
+        text: tb.innerText,
+        left: tb.style.left,
+        top: tb.style.top,
+        fontFamily: tb.style.fontFamily,
+        fontSize: tb.style.fontSize,
+        color: tb.style.color,
+        fontWeight: tb.style.fontWeight,
+        fontStyle: tb.style.fontStyle,
+        textDecoration: tb.style.textDecoration
+    }));
 
     const predefinedTextBox = document.querySelector('.text-box.predefined');
     if (predefinedTextBox) {
-        slides[currentImageIndex].left = predefinedTextBox.style.left;
-        slides[currentImageIndex].top = predefinedTextBox.style.top;
+        updateTextBoxData(predefinedTextBox, slides[currentImageIndex].predefinedText);
     }
 }
 
@@ -112,60 +151,61 @@ document.getElementById("nextSlide").addEventListener("click", () => {
     saveCurrentTextBoxes();
     currentImageIndex = (currentImageIndex + 1) % slides.length;
     updateSlide();
+    saveState();
 });
 
 document.getElementById("prevSlide").addEventListener("click", () => {
     saveCurrentTextBoxes();
     currentImageIndex = (currentImageIndex - 1 + slides.length) % slides.length;
     updateSlide();
+    saveState();
 });
 
-updateSlide();
-
 function saveState() {
-    const currentState = {
+    const currentState = JSON.parse(JSON.stringify({
         slideIndex: currentImageIndex,
-        textBoxes: slides[currentImageIndex].textBoxes.map(textBox => ({
-            innerText: textBox.innerText,
-            left: textBox.left,
-            top: textBox.top,
-            fontFamily: textBox.fontFamily,
-            fontSize: textBox.fontSize
-        })),
-        predefinedText: slides[currentImageIndex].text
-    };
+        slides: slides
+    }));
     history.push(currentState);
     if (history.length > 100) history.shift();
     redoStack = [];
+    updateUndoRedoButtons();
+}
+
+function applyState(state) {
+    currentImageIndex = state.slideIndex;
+    slides = state.slides;
+    updateSlide();
+    updateUndoRedoButtons();
+}
+
+function updateUndoRedoButtons() {
+    undoButton.disabled = history.length <= 1;
+    redoButton.disabled = redoStack.length === 0;
 }
 
 addTextButton.addEventListener("click", () => {
     let textBox = {
-        innerText: "New Text",
+        text: "New Text",
         fontFamily: fontFamilySelect.value,
         fontSize: fontSizeInput.value + "px",
+        color: textColorInput.value,
         left: "10px",
-        top: "10px"
+        top: "10px",
+        fontWeight: "normal",
+        fontStyle: "normal",
+        textDecoration: "none"
     };
     
     let newTextBoxElement = document.createElement("div");
     newTextBoxElement.contentEditable = true;
     newTextBoxElement.className = "text-box";
-    newTextBoxElement.innerText = textBox.innerText;
-    newTextBoxElement.style.fontFamily = textBox.fontFamily;
-    newTextBoxElement.style.fontSize = textBox.fontSize;
+    Object.assign(newTextBoxElement.style, textBox);
+    newTextBoxElement.innerText = textBox.text;
     newTextBoxElement.style.position = "absolute";
-    newTextBoxElement.style.left = textBox.left; 
-    newTextBoxElement.style.top = textBox.top; 
     imageContainer.appendChild(newTextBoxElement);
     
-    slides[currentImageIndex].textBoxes.push({
-        innerText: textBox.innerText,
-        fontFamily: textBox.fontFamily,
-        fontSize: textBox.fontSize,
-        left: textBox.left,
-        top: textBox.top
-    });
+    slides[currentImageIndex].textBoxes.push(textBox);
     
     makeElementDraggable(newTextBoxElement);
 
@@ -176,6 +216,7 @@ addTextButton.addEventListener("click", () => {
 
     newTextBoxElement.addEventListener("input", () => {
         saveState();
+        updateTextBoxData(newTextBoxElement, textBox);
     });
 
     saveState();
@@ -221,8 +262,9 @@ function makeElementDraggable(element, isPredefined = false) {
         element.style.left = newX + "px";
         element.style.top = newY + "px";
 
-        if (!isPredefined) {
-            saveState();
+        if (isPredefined) {
+            slides[currentImageIndex].predefinedText.left = newX + "px";
+            slides[currentImageIndex].predefinedText.top = newY + "px";
         }
     }
 
@@ -250,12 +292,14 @@ function updateControls() {
         boldButton.classList.toggle("active", selectedElement.style.fontWeight === "bold");
         italicButton.classList.toggle("active", selectedElement.style.fontStyle === "italic");
         underlineButton.classList.toggle("active", selectedElement.style.textDecoration === "underline");
+        textColorInput.value = selectedElement.style.color || "#000000";
     } else {
         fontFamilySelect.value = "Arial";
         fontSizeInput.value = 16;
         boldButton.classList.remove("active");
         italicButton.classList.remove("active");
         underlineButton.classList.remove("active");
+        textColorInput.value = "#000000";
     }
 }
 
@@ -300,21 +344,18 @@ underlineButton.addEventListener("click", () => {
     }
 });
 
+textColorInput.addEventListener("input", () => {
+    if (selectedElement) {
+        selectedElement.style.color = textColorInput.value;
+        saveState();
+    }
+});
+
 undoButton.addEventListener("click", () => {
-    if (history.length > 0) {
+    if (history.length > 1) {
         redoStack.push(history.pop());
         const lastState = history[history.length - 1];
-        if (lastState) {
-            currentImageIndex = lastState.slideIndex;
-            slides[currentImageIndex].textBoxes = lastState.textBoxes.map(textBox => ({
-                innerText: textBox.innerText,
-                left: textBox.left,
-                top: textBox.top,
-                fontFamily: textBox.fontFamily,
-                fontSize: textBox.fontSize
-            }));
-            updateSlide();
-        }
+        applyState(lastState);
     }
 });
 
@@ -322,14 +363,84 @@ redoButton.addEventListener("click", () => {
     if (redoStack.length > 0) {
         const redoState = redoStack.pop();
         history.push(redoState);
-        currentImageIndex = redoState.slideIndex;
-        slides[currentImageIndex].textBoxes = redoState.textBoxes.map(textBox => ({
-            innerText: textBox.innerText,
-            left: textBox.left,
-            top: textBox.top,
-            fontFamily: textBox.fontFamily,
-            fontSize: textBox.fontSize
-        }));
-        updateSlide();
+        applyState(redoState);
     }
 });
+
+shuffleButton.addEventListener("click", () => {
+    showReorderModal();
+});
+
+function showReorderModal() {
+    const modal = document.createElement("div");
+    modal.className = "reorder-modal";
+    
+    const modalContent = document.createElement("div");
+    modalContent.className = "reorder-modal-content";
+    
+    const title = document.createElement("h2");
+    title.textContent = "Reorder Images";
+    modalContent.appendChild(title);
+    
+    const imageContainer = document.createElement("div");
+    imageContainer.className = "reorder-image-container";
+    
+    slides.forEach((slide, index) => {
+        const imageWrapper = document.createElement("div");
+        imageWrapper.className = "reorder-image-wrapper";
+        
+        const img = document.createElement("img");
+        img.src = slide.src;
+        img.alt = `Slide ${index + 1}`;
+        
+        const textOverlay = document.createElement("div");
+        textOverlay.className = "reorder-text-overlay";
+        textOverlay.textContent = slide.predefinedText.text;
+        
+        imageWrapper.appendChild(img);
+        imageWrapper.appendChild(textOverlay);
+        imageWrapper.draggable = true;
+        
+        imageWrapper.addEventListener("dragstart", (e) => {
+            e.dataTransfer.setData("text/plain", index);
+        });
+        
+        imageWrapper.addEventListener("dragover", (e) => {
+            e.preventDefault();
+        });
+        
+        imageWrapper.addEventListener("drop", (e) => {
+            e.preventDefault();
+            const fromIndex = parseInt(e.dataTransfer.getData("text/plain"));
+            const toIndex = index;
+            if (fromIndex !== toIndex) {
+                const [movedSlide] = slides.splice(fromIndex, 1);
+                slides.splice(toIndex, 0, movedSlide);
+                imageContainer.innerHTML = "";
+                showReorderModal();
+            }
+        });
+        
+        imageContainer.appendChild(imageWrapper);
+    });
+    
+    modalContent.appendChild(imageContainer);
+    
+    const closeButton = document.createElement("button");
+    closeButton.textContent = "Close";
+    closeButton.addEventListener("click", () => {
+        document.body.removeChild(modal);
+        currentImageIndex = 0;
+        saveState();
+        updateSlide();
+    });
+    
+    modalContent.appendChild(closeButton);
+    modal.appendChild(modalContent);
+    document.body.appendChild(modal);
+}
+
+// Initialize the first state
+saveState();
+updateSlide();
+updateUndoRedoButtons();
